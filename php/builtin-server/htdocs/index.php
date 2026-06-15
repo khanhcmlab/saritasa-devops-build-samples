4034306e8d7dd9cf1152c739f4ea53044d5db757
dewwripper <deww.ripper@gmail.com>
2026-06-15 02:45:33 +0000

    chore: revert test component

--- php/builtin-server/htdocs/index.php
+++ php/builtin-server/htdocs/index.php
@@ -8,5 +8,4 @@
 echo '<img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>';
 ?>
   </body>
-  <!-- test change to trigger build -->
 </html>
